1bc60254f7eb1526bf5bbbab4abe9a1793ef511a
pazorx <mohsenhshn@Gmail.com>
2023-07-13 23:40:04 +0300

    Cards
    I have made 2 cards
    Elephant Seal
    Hard Worker Gus

--- StickerBattles/Assets/Scripts/Card.cs
+++ StickerBattles/Assets/Scripts/Card.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
+
 [System.Serializable]
 
 /*health
@@ -18,14 +20,14 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public int id;
-    public int health;
-    public int attack;
-    public int defence;
-    public string cardname;
-    public string carddescription;
-    public string magic;
-    public string magicdescription;
+    public int id;1
+    public int health;12
+    public int attack;5
+    public int defence;2
+    public string cardname;Elephant Seal
+    public string carddescription;(A FAT UGLY seal that lives in the deep ocean and has a powerful attack and great indurance.)
+    public string magic; Obesity
+    public string magicdescription;(Decreases the infected card's Stats by 1.)
 
     public Card()
     {
@@ -34,14 +36,14 @@ public class Card : MonoBehaviour
 
     public Card(int Id,int Health,int Attack,int Defence,string CardName, string CardDescription, string Magic, string MagicDescription)
     {
-        id = Id;
-        health = Health;
-        attack = Attack;
-        defence = Defence;
-        cardname = CardName;
-        carddescription = CardDescription;
-        magic = Magic;
-        magicdescription = MagicDescription;
+        id = Id;2
+        health = Health;12
+        attack = Attack;6
+        defence = Defence;2
+        cardname = CardName;Hard Working Gus
+        carddescription = CardDescription;(Hard Worker Gus has been working on his bus for a long time, one day he wi)
+        magic = Magic;Helmet
+        magicdescription = MagicDescription;(Increases Defence by 5 for 3 turns.)
     }
 
 
