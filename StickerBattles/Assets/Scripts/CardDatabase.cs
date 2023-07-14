2e761606a91dcbcde39b9d960904cfafa69c2fcc
DraizaClaw <zayood.asfour@icloud.com>
2023-07-14 11:40:03 +0300

    Small fixes
    edited the card script
    made a scene called deck
    this is where @PAZORX will make the card template

--- StickerBattles/Assets/Scripts/CardDatabase.cs
+++ StickerBattles/Assets/Scripts/CardDatabase.cs
@@ -11,9 +11,9 @@ public class CardDatabase : MonoBehaviour
     {
 
         cardlist.Add(new Card(0, 0, 0, 0, "None", "None", "None", "None"));
-        cardlist.Add(new Card(1, 2, 5, 1, "Card1", "This has id number 1", "run", "This is a random thing"));
-        cardlist.Add(new Card(2, 5, 1, 5, "Card2", "This has id number ", "lie", "This is a random thing"));
-        cardlist.Add(new Card(3, 3, 3, 3, "Card3", "This has id number ", "die", "This is a random thing"));
+        cardlist.Add(new Card(1, 2, 5, 1, "Card1", "This has id number 1", "run", "This is a random thing run "));
+        cardlist.Add(new Card(2, 5, 1, 5, "Card2", "This has id number ", "lie", "This is a random thing lie "));
+        cardlist.Add(new Card(3, 3, 3, 3, "Card3", "This has id number ", "die", "This is a random thing die "));
 
 
 
